4025598682be946497a48e3648d53691ffda0788
shandriy <entyspite@gmail.com>
2024-06-23 09:47:52 -0700

    Amount of raindrops changes with screen width

--- rain.js
+++ rain.js
@@ -1,22 +1,26 @@
 addEventListener("DOMContentLoaded", () => {
-  let canvas = document.getElementById("rain");
-  let context = canvas.getContext("2d", { alpha: false });
+  const canvas = document.getElementById("rain");
+  const context = canvas.getContext("2d", { alpha: false });
   let now = performance.now(), delta;
   let raindrops = [];
   let width = canvas.width, height = canvas.height;
-  let i = 500;
-  while (i > 0) {
-    raindrops.push([Math.random(), Math.random(), Math.random(), Math.random() + 0.1]);
-    i--;
+  function generate(amount) {
+    let i = amount;
+    while (i > 0) {
+      raindrops.push([Math.random(), Math.random(), Math.random(), Math.random() + 0.1]);
+      i--;
+    }
   }
+  generate(Math.round(width / 6));
   function rain() {
     delta = performance.now() - now;
+    document.getElementById("article").innerHTML = delta;
     now += delta;
     context.fillStyle = "#9394a3";
     context.fillRect(0, 0, width, height);
-    context.lineWidth = width * 0.001;
     context.strokeStyle = "#fff";
     raindrops.forEach((drop, index) => {
+      context.lineWidth = 1.5 * (0.5 + (drop[2] * 1.5));
       context.beginPath();
       context.moveTo(drop[0] * width, drop[1] * height);
       context.lineTo(drop[0] * width, (drop[1] + (0.1 * drop[3])) * height);
@@ -35,10 +39,24 @@ addEventListener("DOMContentLoaded", () => {
   function resize() {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
+    if (canvas.width < width) {
+      let i = Math.round(width / 6) - Math.round(canvas.width / 6);
+      while (i > 0) {
+        raindrops.pop();
+        i--;
+      }
+    } else {
+      generate(Math.round(canvas.width / 6) - Math.round(width / 6));
+    }
     width = canvas.width;
     height = canvas.height;
-    rain();
+    //rain();
   }
   onresize = resize;
+  onfocus = () => {
+    raindrops = [];
+    now = performance.now();
+    generate(Math.round(width / 6));
+  };
   resize();
 })
