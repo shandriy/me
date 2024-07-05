8a74940411214ab194aa9a16245df4b989deabfe
shandriy <entyspite@gmail.com>
2024-07-04 20:30:46 -0700

    Reset again...

--- build/min.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $(dirname ${BASH_SOURCE[0]})/.. &> /dev/null
-echo "/*! Konstantin Edunov 2024, compiled $(date) with mishoo/UglifyJS on GitHub */" > output/main.js
-uglifyjs $(find scripts/* -type f) -c hoist_funs,hoist_vars -m reserved=[RETAIN] --mangle-props --toplevel --webkit >> output/main.js
--- build/true.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $(dirname ${BASH_SOURCE[0]})/.. &> /dev/null
-uglifyjs $(find scripts/* -type f) -c hoist_funs,hoist_vars --toplevel --comments all --beautify --webkit > output/main.js
