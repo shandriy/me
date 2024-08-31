d03e6d3819aa9c5a7554afcd3f0c198143b6e014
shandriy <151408521+shandriy@users.noreply.github.com>
2024-08-31 12:56:54 -0700

    Reset to website

--- scripts/defs/11.js
+++
@@ -1,8 +0,0 @@
-console.log("2011");
-var context;
-{
-  var CANVAS = document.getElementById("canvas");
-  context =
-    CANVAS.getContext("webgl", { alpha: false })
-    || CANVAS.getContext("experimental-webgl", { alpha: false });
-}
--- scripts/defs/9.js
+++
@@ -1,2 +0,0 @@
-console.log("2009");
-var context = document.getElementById("canvas").getContext("2d", { alpha: false });
