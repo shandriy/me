220e207e2b2a6bf2ab43c2f24600591fc952cbda
shandriy <151408521+shandriy@users.noreply.github.com>
2024-08-31 22:20:49 -0700

    Add movable windows

---
+++ main.js
@@ -0,0 +1,38 @@
+addEventListener("DOMContentLoaded", function() {
+  var windows = document.getElementsByClassName("window");
+  var length = windows.length;
+  for (var i = 0; i < length; i += 1) {
+    var windowDiv = windows[i];
+    var down = false;
+    var top = 0;
+    var left = 0;
+    var offsetX = 0;
+    var offsetY = 0;
+    windowDiv.addEventListener("mousedown", function(event) {
+      down = true;
+      var top = parseFloat(windowDiv.style.top);
+      var left = parseFloat(windowDiv.style.left);
+      if (isNaN(top) || isNaN(left)) {
+        windowDiv.style.top = windowDiv.offsetTop + "px";
+        windowDiv.style.left = windowDiv.offsetLeft + "px";
+        top = windowDiv.offsetTop;
+        left = windowDiv.offsetLeft;
+      };
+      offsetY = top - event.pageY;
+      offsetX = left - event.pageX;
+    });
+    addEventListener("mouseup", function() {
+      down = false;
+    });
+    addEventListener("mousemove", function(event) {
+      if (down) {
+        var y = event.pageY + offsetY;
+        var x = event.pageX + offsetX;
+        windowDiv.style.top = y + "px";
+        windowDiv.style.left = x + "px";
+        top = y;
+        left = x;
+      };
+    });
+  };
+});
