111d47d75c2bc100ba31c18b7b12c2cf0b0f2f81
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-09 13:40:24 -0800

    New build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set working directory to script directory
 cd $(dirname $0)
@@ -8,6 +8,7 @@ BUILD_DIR=build
 WORKING_DIR=working
 SOURCE_DIR=src
 TEMPLATE_DIR=template
+STATIC_DIR=static
 
 # Set working directory relative to build
 # directory
@@ -28,6 +29,28 @@ find $SOURCE_DIR > $WORKING_DIR/lists.txt
 
 # If file is HTML, just move it to the
 # build direcoty
-while read path; do
-  echo "$path"
+while read path
+do
+  REL=$(realpath -m --relative-to=$SOURCE_DIR $path)
+  if [[ -d $path ]]
+  then
+    if [ ! -d $BUILD_DIR/$REL ]
+    then
+      mkdir $BUILD_DIR/$REL
+    fi
+  else
+    echo $path
+    if [[ $path == *.md ]]
+    then
+      cat $TEMPLATE_DIR/pre.htm > $BUILD_DIR/$REL
+      pandoc $path >> $BUILD_DIR/$REL
+      cat $TEMPLATE_DIR/post.htm >> $BUILD_DIR/$REL
+      new_path=$BUILD_DIR/$REL
+      mv $BUILD_DIR/$REL "${new_path%.*}.htm"
+    else
+      cp $path $BUILD_DIR/$REL
+    fi
+  fi
 done < $WORKING_DIR/lists.txt
+
+cp -r $STATIC_DIR $BUILD_DIR/$STATIC_DIR
