2402f287dd2d4d32fd5320610ebc42b34b55694d
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-14 18:21:53 -0800

    Move songs to separate script

--- config.sh
+++ config.sh
@@ -12,11 +12,10 @@ FILE_BUILD=./every.sh
 # Custom variables
 
 SECONDS=$(date +"%s")
-
-song_of_the_day_name="Guess featuring billie eilish"
-song_of_the_day_album="Brat but it's completely different but also still brat"
-song_of_the_day_artist="Charli xcx"
-song_of_the_day_spotify="https://open.spotify.com/track/0IsIY8pfu1yaGkPUD7pkDx?si=f5bab50966014e4f"
-song_of_the_day_youtube="https://youtu.be/huGd4efgdPA?si=HFG69GFPU-jB_Fek"
-
 FOOTER_TEXT=$(cat ./src/footer-text.htm)
+
+SONG="Guess_featuring_billie_eilish"
+
+# Get song script
+
+. "music/$SONG.sh"
---
+++ music/Guess_featuring_billie_eilish.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Guess featuring billie eilish"
+song_of_the_day_album="Brat but it's completely different but also still brat"
+song_of_the_day_artist="Charli xcx"
+song_of_the_day_spotify="https://open.spotify.com/track/0IsIY8pfu1yaGkPUD7pkDx?si=f5bab50966014e4f"
+song_of_the_day_youtube="https://youtu.be/huGd4efgdPA?si=HFG69GFPU-jB_Fek"
